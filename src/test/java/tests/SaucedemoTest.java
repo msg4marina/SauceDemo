a9e2a0db64c2cac1add28769199977fc7313891e
Marina Maslova <msg4marina@gmail.com>
2021-05-29 20:03:34 +0300

    SaucedemoTest:повторен Description после reload

--- src/test/java/tests/SaucedemoTest.java
+++ src/test/java/tests/SaucedemoTest.java
@@ -1,6 +1,8 @@
 package tests;
+
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+
+import static org.testng.Assert.assertEquals;
 
 public class SaucedemoTest extends BaseTest {
     @Test
@@ -8,22 +10,15 @@ public class SaucedemoTest extends BaseTest {
         loginPage.open();
         loginPage.Login(USER, PASSWORD);
     }
-    @Test(description = "login should be correct")
-    public void loginShouldBeCorrect(String user, String passsword, String errorMessage) {
-        loginPage.open();
-        loginPage.Login(USER, PASSWORD);
-        boolean isError = loginPage.isErrorDisplayed();
-        assertFalse(isError, "gbggdgdfg");
-        boolean opened = productsPage.isPageOpened();
-        assertTrue(opened, "dlnsdjksjkdbsdjwbjbq2t");
-    }
-    @Test(retryAnalyzer = Retry.class, description = "username should be required")
+
+    @Test(description = "user name should be required")
     public void userNameShouldBeRequired( ) {
         loginPage.open();
         loginPage.Login("", PASSWORD);
         String error = loginPage.getError();
         assertEquals(error, "Epic sadface: Username is required", "Error message is not correct");
     }
+
     @Test(description = "password should be required")
     public void passwordShouldBeRequired( ) {
         loginPage.open();
@@ -31,16 +26,18 @@ public class SaucedemoTest extends BaseTest {
         String error = loginPage.getError();
         assertEquals(error, "Epic sadface: Password is required", "Password should be written");
     }
+
     @Test(description = "username should be written as specified")
-    public void usernamedShouldBeWrittenAsSpecified( ) {
+    public void usernameShouldWrittenAsSpecified( ) {
         loginPage.open();
         loginPage.Login("fdfdfsfsf4334324&@", PASSWORD);
         String error = loginPage.getError();
         assertEquals(error, "Epic sadface: Username and password do not match any user in this service",
                 "Username should be according to specified");
     }
-    @Test(description = "password should be written as specified")
-    public void passwordShouldBeWrittenAsSpecified( ) {
+
+    @Test(description = "password should written as specified")
+    public void passwordShouldWrittenAsSpecified( ) {
         loginPage.open();
         loginPage.Login(USER, "fdfdfsfsf4334324&@");
         String error = loginPage.getError();
