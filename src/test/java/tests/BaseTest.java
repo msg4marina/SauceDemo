663e7c0ba790b986eb3dbe63523105defa0e8369
Marina Maslova <msg4marina@gmail.com>
2021-05-29 14:12:51 +0300

    @Test заменен на @Listeners(TestListener.class)

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,17 +1,18 @@
 package tests;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import pages.CartPage;
 import pages.LoginPage;
 import pages.ProductsPage;
-
 import java.util.concurrent.TimeUnit;
 
+@Listeners(TestListener.class)
 public abstract class BaseTest {
     WebDriver driver;
     LoginPage loginPage;
@@ -20,20 +21,30 @@ public abstract class BaseTest {
     public static final String USER = "standard_user";
     public static final String PASSWORD = "secret_sauce";
 
+    @Parameters({"browser"})
     @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // неявные ожидания
-        loginPage = new LoginPage(driver);
-        productsPage = new ProductsPage(driver);
-        cartPage = new CartPage(driver);
+    public void setUp(@Optional("chrome") String browser, ITestContext testContext) {
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+        } else if (browser.equals("opera")) {
+            WebDriverManager.operadriver().setup();
+            OperaOptions options = new OperaOptions();
+            options.addArguments("--start-maximized");
+            driver = new OperaDriver(options);
+
+
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // неявные ожидания
+            loginPage = new LoginPage(driver);
+            productsPage = new ProductsPage(driver);
+            cartPage = new CartPage(driver);
+        }
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() {
-        driver.quit();
+        @AfterMethod(alwaysRun = true)
+        public void tearDown() {
+            driver.quit();
+        }
     }
-}
