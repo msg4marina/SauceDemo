1978896dc20d7709c5c47ffd0cd46c915c404eec
Marina Maslova <msg4marina@gmail.com>
2021-06-05 11:13:10 +0300

    -comment: захардкодила название продукта -fixed by: REPLACE:  String addToCartButton = "//*[contains(text(), 'Sauce Labs Backpack')]//ancestor::*[@class='inventory_item']//button"; WITH: String addToCartButton = "//*[contains(text(), '%s')]//ancestor::*[@class='inventory_item']//button";

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProductsPage extends BasePage {
-    String addToCartButton = "//*[contains(text(), 'Sauce Labs Backpack')]//ancestor::*[@class='inventory_item']//button";
+    String addToCartButton = "//*[contains(text(), '%s')]//ancestor::*[@class='inventory_item']//button";
 
     public ProductsPage(WebDriver driver) {
         super(driver);
