ccdad0a6e13a0d2837104b455657f20597dbbc12
Marina Maslova <msg4marina@gmail.com>
2021-06-05 23:54:16 +0300

    -comment: тут ты выставляешь testContext, а для хрома - нет.Советую вынести из IF его ниже. - fixed: "testContext.setAttribute("driver", driver);" shifted under "cartPage = new CartPage(driver);" (AFTER "IF")

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,9 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import pages.CartPage;
 import pages.LoginPage;
 import pages.ProductsPage;
@@ -22,19 +23,27 @@ public abstract class BaseTest {
     public static final String USER = "standard_user";
     public static final String PASSWORD = "secret_sauce";
 
+    @Parameters ({"browser"})
     @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+    public void setUp(@Optional("chrome") String browser, ITestContext testContext) {
+        System.out.println(browser);
+        if (browser.equals("chrome")){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        } else if (browser.equals("opera")){
+            WebDriverManager.operadriver().setup();
+           OperaOptions options = new OperaOptions();
+            driver = new OperaDriver(options);
+        }
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
+        testContext.setAttribute("driver", driver);
     }
-
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit();
