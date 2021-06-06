41bae9cd6c5ec5f4ae8c1ac54d11ea870eb54e79
Marina Maslova <msg4marina@gmail.com>
2021-06-06 20:49:52 +0300

    added allure & crossbrowser

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,14 +4,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
 import pages.CartPage;
 import pages.LoginPage;
 import pages.ProductsPage;
 import tests.base.TestListener;
 
+import java.nio.charset.MalformedInputException;
 import java.util.concurrent.TimeUnit;
 
 @Listeners(TestListener.class)
@@ -24,12 +29,22 @@ public abstract class BaseTest {
     public static final String PASSWORD = "secret_sauce";
 
     @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // неявные ожидания
+    public void setUp(@Optional("chrome") String browser, ITestContext testContext) throws MalformedInputException {
+        System.out.println(browser);
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+
+        } else if (browser.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            EdgeOptions options = new EdgeOptions();
+            driver = new EdgeDriver(options);
+        }
+        testContext.setAttribute("driver", driver);
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
