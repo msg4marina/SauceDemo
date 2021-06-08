1c11346233f0cff2eaa9fdf1193f0a595fb056ad
Marina Maslova <msg4marina@gmail.com>
2021-06-08 21:27:31 +0300

    added headless

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -35,6 +35,7 @@ public abstract class BaseTest {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--start-maximized");
+            options.addArguments("--headless");
             driver = new ChromeDriver(options);
 
         } else if (browser.equals("edge")) {
