4aa1a625a8f188625416703910bb513bc567efc5
Marina Maslova <msg4marina@gmail.com>
2021-06-13 18:29:48 +0300

    added @Log4j

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,9 +1,11 @@
 package pages;
 
+import lombok.extern.log4j.Log4j;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+@Log4j
 public class ProductsPage extends BasePage {
     String addToCartButton = "//*[contains(text(), '%s')]//ancestor::*[@class='inventory_item']//button";
 
@@ -14,5 +16,7 @@ public class ProductsPage extends BasePage {
     @Step("Add product: {name} into cart")
     public void addToCart(String name) {
         driver.findElement(By.xpath(String.format(addToCartButton, name))).click();
+        log.warn("warn");
+        log.info("info");
     }
 }
