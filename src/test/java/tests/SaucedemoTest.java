cdeba7a9ed3caa8c0e13f658d14e7c09722d30be
Marina Maslova <msg4marina@gmail.com>
2021-06-13 14:47:40 +0300

    annotation @Step has been added

--- src/test/java/tests/SaucedemoTest.java
+++ src/test/java/tests/SaucedemoTest.java
@@ -1,21 +1,23 @@
 package tests;
 
+import io.qameta.allure.Step;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class SaucedemoTest extends BaseTest {
-    @Test
-    public void testSauceDemoSite( ) {
+    @Step ("Open Saucedemo site")
+    public void testSauceDemoSite() {
         loginPage.open();
         loginPage.Login(USER, PASSWORD);
     }
-    @Test(description = "login")
-    public void login( ) {
+    @Step ("Open Login page")
+    public void login() {
         loginPage.open();
         loginPage.Login(USER, PASSWORD);
     }
+    @Step ("Insert the data into the Login page fields")
     @DataProvider(name = "Login Data")
     public Object[][] getLoginData( ) {
         return new Object[][]{
@@ -26,6 +28,7 @@ public class SaucedemoTest extends BaseTest {
                 {USER, "fdfdfsfsf4334324&@", "Epic sadface: Username and password do not match any user in this service"},
         };
     }
+    @Step ("Check that user name  and password should be required in Login")
     @Test(description = "user name  and password should be required", dataProvider = "Login Data")
     public void userNameShouldBeRequired(String user, String password, String errorMessage) {
         loginPage.open();
