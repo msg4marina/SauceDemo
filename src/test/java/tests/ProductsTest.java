7c5a4c61e3642c14db4cc775feb9ce9199fe5aa7
Marina Maslova <msg4marina@gmail.com>
2021-06-13 14:50:22 +0300

    annotation @Step has been added

--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -1,9 +1,11 @@
 package tests;
 
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 import tests.base.Retry;
 
 public class ProductsTest extends BaseTest {
+    @Step ("Check the product adding into the cart")
     @Test(retryAnalyzer = Retry.class, description = "product should be added into the cart")
     public void productShouldBeAddedIntoTheCart( ) {
         loginPage.open();
