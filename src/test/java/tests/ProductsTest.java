d661fa7eec2d7972648a68bc8f5f513c52d7479d
Marina Maslova <msg4marina@gmail.com>
2021-06-13 18:22:49 +0300

    added @Log4j

--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -1,17 +1,24 @@
 package tests;
 
+import lombok.extern.log4j.Log4j;
 import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 import tests.base.Retry;
 
+@Log4j
 public class ProductsTest extends BaseTest {
     @Step ("Check the product adding into the cart")
     @Test(retryAnalyzer = Retry.class, description = "product should be added into the cart")
+
     public void productShouldBeAddedIntoTheCart( ) {
         loginPage.open();
         loginPage.Login(USER, PASSWORD);
         productsPage.addToCart("Test.allTheThings() T-Shirt (Red)");
         cartPage.open("shopping_cart_link");
         //TODO VALIDATE NAME AND PRICE
+        log.fatal("fatal");
+        log.error("error");
+        log.debug("debug");
+
     }
 }
